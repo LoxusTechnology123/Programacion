1f8eb43403cd064ab0a40a983404da6991eef4b6
LoxusTechnology123 <66702265+LoxusTechnology123@users.noreply.github.com>
2020-08-31 21:36:09 -0300

    Add files via upload

---
+++ Persistencia/ConexionSql.vb
@@ -0,0 +1,15 @@
+﻿'Imports MySql.Data.MySqlClient
+'Public Class ConexionSql
+'Dim conn As MySqlConnection
+'Public Sub conexion()
+'Try
+'           conn = New MySqlConnection("server=localhost; userid=root; password=; database=MyCompany")
+'conn.Open()
+'Catch ex As Exception
+'MsgBox("ERROR" & ex.Message)
+'End Try
+'End Sub
+'ublic Sub cerrarconexion()
+'Me.conn.Close()
+'End Sub
+'End Class
