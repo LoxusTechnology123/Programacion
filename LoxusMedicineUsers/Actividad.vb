af499d82e228eb70d74b152b77a5fc484cd3a640
LoxusTechnology123 <66702265+LoxusTechnology123@users.noreply.github.com>
2020-08-31 21:35:13 -0300

    Add files via upload

---
+++ LoxusMedicineUsers/Actividad.vb
@@ -0,0 +1,22 @@
+﻿Imports MySql.Data.MySqlClient
+Public Class Actividad
+    Dim MysqlConn As MySqlConnection
+    Dim datos As DataSet
+    Dim adaptador As New MySqlDataAdapter
+    Private Sub Actividad_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Try
+            MysqlConn = New MySqlConnection 'Crea la conexion
+            MysqlConn.ConnectionString = "server=localhost; userid=root; password=; database=MyCompany" 'Especifica a donde se va a conectar
+            MysqlConn.Open() 'Abre la conexion
+            Dim consulta As String
+            consulta = "select nombre_patologia * from patologiassintomas"
+            adaptador = New MySqlDataAdapter(consulta, MysqlConn)
+            datos = New DataSet
+            adaptador.Fill(datos, "patologiassintomas")
+            GridPatologias.DataSource = datos
+            GridPatologias.DataMember = "patologiassintomas"
+        Catch ex As Exception
+
+        End Try
+    End Sub
+End Class
