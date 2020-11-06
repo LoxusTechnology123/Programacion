c1537ffc34745f889c42908a3137d18a65866f49
LoxusTechnology123 <66702265+LoxusTechnology123@users.noreply.github.com>
2020-11-05 23:03:53 -0300

    Add files via upload

---
+++ Dominio/LogicalControl.vb
@@ -0,0 +1,13 @@
+﻿Public Class LogicalControl
+    Private Property mensajes As New List(Of Message)
+    Public Shared Property instancia As New LogicalControl
+
+    Public Function Obtenermensajes() As List(Of Message)
+        Return mensajes
+    End Function
+
+    Public Sub AgregarMensaje(mensaje As Message)
+        mensajes.Add(mensaje)
+    End Sub
+
+End Class
---
+++ Dominio/Message.vb
@@ -0,0 +1,13 @@
+﻿Public Class Message
+    Public Property Cipersona As String
+    Public Property Nombre As String
+    Public Property Texto As String
+    Public Property apellido As String
+
+    Public Sub New(pCipersona As String, pTexto As String, pnombre As String, papellido As String)
+        Me.Cipersona = pCipersona
+        Me.Texto = pTexto
+        Me.Nombre = pnombre
+        Me.apellido = papellido
+    End Sub
+End Class
