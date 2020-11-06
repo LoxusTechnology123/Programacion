ea8559601d78a09b66eb21712f6451b8abf9976b
LoxusTechnology123 <66702265+LoxusTechnology123@users.noreply.github.com>
2020-11-05 23:05:51 -0300

    Add files via upload

---
+++ Persistencia/EmailServices/LoxusSupportMail.vb
@@ -0,0 +1,17 @@
+﻿Public Class LoxusSupportMail
+    Inherits MasterEmailServices
+
+    Public Sub New()
+        senderMail = "soporteloxus@gmail.com"
+        password = "Soporte123"
+        host = "smtp.gmail.com"
+        port = 587
+        ssl = True
+        InitializeSmtpClient()
+    End Sub
+
+    Public Sub Noti()
+
+    End Sub
+
+End Class
